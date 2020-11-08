a781d9b8095bed8cd069acadf94fcfb57bea034c
denis-shvetcov <deniska28082001@gmail.com>
2020-11-09 00:59:39 +0700

    09.11.2020 6 урок без теста на цикл

--- src/lesson6/JavaGraphTasks.java
+++ src/lesson6/JavaGraphTasks.java
@@ -2,34 +2,33 @@ package lesson6;
 
 import kotlin.NotImplementedError;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaGraphTasks {
     /**
      * Эйлеров цикл.
      * Средняя
-     *
+     * <p>
      * Дан граф (получатель). Найти по нему любой Эйлеров цикл.
      * Если в графе нет Эйлеровых циклов, вернуть пустой список.
      * Соседние дуги в списке-результате должны быть инцидентны друг другу,
      * а первая дуга в списке инцидентна последней.
      * Длина списка, если он не пуст, должна быть равна количеству дуг в графе.
      * Веса дуг никак не учитываются.
-     *
+     * <p>
      * Пример:
-     *
-     *      G -- H
-     *      |    |
+     * <p>
+     * G -- H
+     * |    |
      * A -- B -- C -- D
      * |    |    |    |
      * E    F -- I    |
      * |              |
      * J ------------ K
-     *
+     * <p>
      * Вариант ответа: A, E, J, K, D, C, H, G, B, C, I, F, B, A
-     *
+     * <p>
      * Справка: Эйлеров цикл -- это цикл, проходящий через все рёбра
      * связного графа ровно по одному разу
      */
@@ -40,25 +39,25 @@ public class JavaGraphTasks {
     /**
      * Минимальное остовное дерево.
      * Средняя
-     *
+     * <p>
      * Дан связный граф (получатель). Найти по нему минимальное остовное дерево.
      * Если есть несколько минимальных остовных деревьев с одинаковым числом дуг,
      * вернуть любое из них. Веса дуг не учитывать.
-     *
+     * <p>
      * Пример:
-     *
-     *      G -- H
-     *      |    |
+     * <p>
+     * G -- H
+     * |    |
      * A -- B -- C -- D
      * |    |    |    |
      * E    F -- I    |
      * |              |
      * J ------------ K
-     *
+     * <p>
      * Ответ:
-     *
-     *      G    H
-     *      |    |
+     * <p>
+     * G    H
+     * |    |
      * A -- B -- C -- D
      * |    |    |
      * E    F    I
@@ -72,83 +71,140 @@ public class JavaGraphTasks {
     /**
      * Максимальное независимое множество вершин в графе без циклов.
      * Сложная
-     *
+     * <p>
      * Дан граф без циклов (получатель), например
-     *
-     *      G -- H -- J
-     *      |
+     * <p>
+     * G -- H -- J
+     * |
      * A -- B -- D
      * |         |
      * C -- F    I
      * |
      * E
-     *
+     * <p>
      * Найти в нём самое большое независимое множество вершин и вернуть его.
      * Никакая пара вершин в независимом множестве не должна быть связана ребром.
-     *
+     * <p>
      * Если самых больших множеств несколько, приоритет имеет то из них,
      * в котором вершины расположены раньше во множестве this.vertices (начиная с первых).
-     *
+     * <p>
      * В данном случае ответ (A, E, F, D, G, J)
-     *
+     * <p>
      * Если на входе граф с циклами, бросить IllegalArgumentException
-     *
+     * <p>
      * Эта задача может быть зачтена за пятый и шестой урок одновременно
      */
     public static Set<Graph.Vertex> largestIndependentVertexSet(Graph graph) {
-        throw new NotImplementedError();
+        if (isCyclic(graph)) throw new IllegalArgumentException();
+        Set<Graph.Vertex> vertices = graph.getVertices();
+        Set<Graph.Vertex> result = new HashSet<>(vertices);
+        for (Graph.Vertex vertex: vertices) {
+            if (!result.contains(vertex)) continue;
+            Set<Graph.Vertex> neighbours = graph.getNeighbors(vertex);
+            for (Graph.Vertex neighbour : neighbours) {
+                System.out.println("Neighbour: " + neighbour);
+            }
+            neighbours.forEach(neighbour-> result.remove(neighbour));
+        }
+        return result;
+    }
+
+
+    private static boolean isCyclic(Graph graph) {
+        Set<Graph.Vertex> vertices = graph.getVertices();
+        List<Graph.Vertex> visited = new ArrayList<>();
+        Stack<Graph.Vertex> stack = new Stack<>();
+        Map<Graph.Vertex,List<Boolean>> vertexCounter = new HashMap<>();
+        for (Graph.Vertex vertex : vertices) {
+            visited.clear();
+            vertexCounter.clear();
+            stack.push(vertex);
+
+            while (!stack.isEmpty()) {
+
+                Set<Graph.Vertex> neighbours = graph.getNeighbors(vertex);
+//                if (visited.contains(stack.peek())) return true;
+                for (Graph.Vertex neighbour : neighbours) {
+                    stack.push(neighbour);
+                    vertexCounter.putIfAbsent(neighbour, new ArrayList<>());
+                    vertexCounter.get(neighbour).add(true);
+                }
+                stack.pop();
+            }
+            // по идее все соседи vertex должны увеличить счетчик на 1, то есть счетчик не должен превышать число
+            // соседей vertex, если же есть цикл, то счетчик будет больше числа соседей vertex.
+        }
+        return false;
     }
 
     /**
      * Наидлиннейший простой путь.
      * Сложная
-     *
+     * <p>
      * Дан граф (получатель). Найти в нём простой путь, включающий максимальное количество рёбер.
      * Простым считается путь, вершины в котором не повторяются.
      * Если таких путей несколько, вернуть любой из них.
-     *
+     * <p>
      * Пример:
-     *
-     *      G -- H
-     *      |    |
+     * <p>
+     * G -- H
+     * |    |
      * A -- B -- C -- D
      * |    |    |    |
      * E    F -- I    |
      * |              |
      * J ------------ K
-     *
+     * <p>
      * Ответ: A, E, J, K, D, C, H, G, B, F, I
      */
     public static Path longestSimplePath(Graph graph) {
-        throw new NotImplementedError();
+        Set<Graph.Vertex> vertices = graph.getVertices();
+        if (vertices.isEmpty()) return new Path();
+        PriorityQueue<Path> queue = new PriorityQueue<>();
+        vertices.forEach(vertex -> queue.add(new Path(vertex)));
+        Path longest = queue.poll();
+        Path currentPath = longest;
+        while ((currentPath = queue.poll()) != null) {
+            List<Graph.Vertex> currentPathVerticles = currentPath.getVertices();
+            Set<Graph.Vertex> neighbours = graph.getNeighbors(currentPathVerticles.get(currentPathVerticles.size() - 1)); // соседи последнего узла в Path
+            for (Graph.Vertex vertex : neighbours) {
+                if (!currentPath.contains(vertex)) { // проверка , не содержится ли узел в Path
+                    Path newPath = new Path(currentPath, graph, vertex);
+                    queue.add(newPath);
+                    if (newPath.getLength() > longest.getLength())
+                        longest = newPath;
+                }
+            }
+        }
+        return longest;
     }
 
 
     /**
      * Балда
      * Сложная
-     *
+     * <p>
      * Задача хоть и не использует граф напрямую, но решение базируется на тех же алгоритмах -
      * поэтому задача присутствует в этом разделе
-     *
+     * <p>
      * В файле с именем inputName задана матрица из букв в следующем формате
      * (отдельные буквы в ряду разделены пробелами):
-     *
+     * <p>
      * И Т Ы Н
      * К Р А Н
      * А К В А
-     *
+     * <p>
      * В аргументе words содержится множество слов для поиска, например,
      * ТРАВА, КРАН, АКВА, НАРТЫ, РАК.
-     *
+     * <p>
      * Попытаться найти каждое из слов в матрице букв, используя правила игры БАЛДА,
      * и вернуть множество найденных слов. В данном случае:
      * ТРАВА, КРАН, АКВА, НАРТЫ
-     *
+     * <p>
      * И т Ы Н     И т ы Н
      * К р а Н     К р а н
      * А К в а     А К В А
-     *
+     * <p>
      * Все слова и буквы -- русские или английские, прописные.
      * В файле буквы разделены пробелами, строки -- переносами строк.
      * Остальные символы ни в файле, ни в словах не допускаются.
